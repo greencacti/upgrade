b2ad9e87892dc77a96b079116659ac65f55f6280
Yiqun Chen <yiqunc@vmware.com>
2015-11-26 19:09:22 +0800

    Add logic to support vCD quiesce timeout case
    vCD quiesce may take a long time and hung. So we will continue to do
    rest things if quiesce hung after the max timeout.

--- src/main/java/com/vmware/cam/tasks/InstallCamUi.java
+++ src/main/java/com/vmware/cam/tasks/InstallCamUi.java
@@ -1,9 +1,13 @@
 package com.vmware.cam.tasks;
 
+import com.vmware.cam.expect.Expecter;
 import com.vmware.cam.util.ScpRcTo;
+import com.vmware.cam.util.SimpleSleep;
 
 import net.sf.expectit.Expect;
+import net.sf.expectit.ExpectIOException;
 import static net.sf.expectit.matcher.Matchers.contains;
+import static net.sf.expectit.matcher.Matchers.sequence;
 import static net.sf.expectit.matcher.Matchers.times;
 
 /**
@@ -16,30 +20,49 @@ public class InstallCamUi {
             scp.scpTo(camUiUrl, remoteCamUiUrl);
             
             //install CAM UI
-            expect.sendLine("rpm -U " + remoteCamUiUrl);
+            expect.sendLine("rpm -U --force " + remoteCamUiUrl);
             expect.expect(contains("Patch completed successfully!"));
             System.out.println("Install CAM UI successfully for " + server);
+            expect.expect(contains("#"));
 
             // Print PID
-            expect.sendLine("cat /var/run/vmware-vcd-cell.pid");
+            expect.sendLine("cat /var/run/vmware-vcd-cell.pid && echo show-cell-pid-1-success");
+            System.out.println(sequence(contains("show-cell-pid-1-success"),contains("show-cell-pid-1-success")));
+            expect.expect(times(2, contains("show-cell-pid-1-success")));
+            expect.expect(contains("#"));
             
             // quiesce on vmware-vcd
             expect.sendLine("/opt/vmware/vcloud-director/bin/cell-management-tool -u " + username + " cell --quiesce true -p '" + password + "' && echo cell-quiesce-success");
-            expect.expect(times(2, contains("cell-quiesce-success")));
-            System.out.println("Quiesce operation succeeded on VCD cell " + server);
+
+            try {
+                expect.expect(times(2, contains("cell-quiesce-success")));
+                expect.expect(contains("#"));
+                System.out.println("Quiesce operation succeeded on VCD cell " + server);
+            }catch(ExpectIOException e){
+                if (e.getMessage().contains("timeout")){
+                    System.out.println(e.getMessage());
+                    System.out.println("Quiesce operation failed to complete in given time on VCD cell " + server + ". Continue to shutdown vcd-cell");
+                }else{
+                    throw e;
+                }
+            }
 
             // shutdown vmware-vcd
             expect.sendLine("/opt/vmware/vcloud-director/bin/cell-management-tool -u " + username + " cell --shutdown -p '" + password + "' && echo cell-shutdown-success");
             expect.expect(times(2, contains("cell-shutdown-success")));
+            expect.expect(contains("#"));
             System.out.println("Shut down VCD cell " + server);
 
             // start vmware-vcd
             expect.sendLine("service vmware-vcd start");
             expect.expect(times(2, contains("OK")));
+            expect.expect(contains("#"));
             System.out.println("Start vCD cell successfully for " + server);
 
             // Print PID
-            expect.sendLine("cat /var/run/vmware-vcd-cell.pid");
+            expect.sendLine("cat /var/run/vmware-vcd-cell.pid && echo show-cell-pid-2-success");
+            expect.expect(times(2, contains("show-cell-pid-2-success")));
+            expect.expect(contains("#"));
       
         } catch (Throwable e) {
             e.printStackTrace();
